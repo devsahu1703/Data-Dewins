a0f8983872ffef39092bd27c0424a418cb1476f9
devsahu1703 <devsahujhs17@gmail.com>
2021-09-09 01:57:08 +0530

    Project

---
+++ project/docker/www/index.php
@@ -0,0 +1,21 @@
+<h1>Hello Datadewins From Group </h1>
+<h4>Attempting MYSQL Connection..</h4>
+<?php
+$host = 'db';
+$user = 'MYSQL_USER';
+$pass = 'MYSQL_PASSWORD';
+$db = 'my_db';
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+}
+
+echo("Server version is: ");
+echo($conn->server_version);
+?>
+
+
+
+
+
